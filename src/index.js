a5c1ce990ea22a34df7695fdccc0a86095abcb1e
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-22 10:44:26 -0400

    fix: Corregir la ruta de importación de reportWebVitals para mantener la estructura del proyecto

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './utilidades/reportWebVitals';
+import reportWebVitals from './logica/utilidades/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
