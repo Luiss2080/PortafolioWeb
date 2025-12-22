a0f884ebca92ef1e68d7c80480ebcf9e78898809
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-22 10:42:39 -0400

    feat: Eliminar hooks personalizados obsoletos para simplificar la lógica del proyecto

--- src/logica/hooks/index.js
+++ src/logica/hooks/index.js
@@ -1,11 +1,5 @@
 // Exportaciones de hooks personalizados
 export { useAnimacionScroll } from './useAnimacionScroll';
-export { useTamañoVentana } from './useTamañoVentana';
 export { useFormulario } from './useFormulario';
-export { useTemaOscuro } from './useTemaOscuro';
-export { useInterseccion } from './useInterseccion';
 export { useGitHub } from './useGitHub';
-export { useLocalStorage } from './useLocalStorage';
-export { useDebounce } from './useDebounce';
-export { useMediaQuery, useEsMovil, useEsTablet, useEsEscritorio } from './useMediaQuery';
 export { useScrollToTop } from './useScrollToTop';
